ba2d0c73da144caf15ecf697fe96d58fad3de511
nathan <cerisara.nathan@gmail.Com>
2020-03-04 16:26:44 +0100

    change logos

--- js/script1.js
+++ js/script1.js
@@ -1,9 +1,35 @@
 
+/*
+            CALCUL DE MON AGE
+*/
+
+
 var age=document.getElementById("datenaissance");
 
 var dt=new Date();
-var monage=parseInt((parseInt(dt.getTime()/1000/3600/24)-(365.25-8))/365.25)
+var monmois=dt.getMonth();
+var monan=dt.getFullYear();
+var monjour=dt.getDate();
 
+//alert(monan+" "+monmois+" "+monjour);
+
+var annais=2003
+var moisnais=11
+var journais=23
+
+var monage=monan-annais
+if(monmois<moisnais){
+    monage-=1;
+}
+else if(monmois==moisnais && monjour<journais){
+    monage-=1;
+}
 
 age.innerHTML=monage
+
+
+/*
+
+*/
+
 
