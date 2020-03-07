9bdd4c0bcc04b2ddcd489ff1030119e9aaa4f326
nathan <cerisara.nathan@gmail.Com>
2020-03-07 19:43:15 +0100

    aa

---
+++ store/jeu.bat
@@ -0,0 +1,19 @@
+
+
+python3 main.py
+
+
+
+python3 main.py
+# Remarque, si le nom du fichier qu'il faut lancer n'est pas 'main.py',
+#veuillez remplacer le 'main.py' par le nom du fichier que vous voulez lancer
+#aussi simple que ca
+
+#si ce n'est pas 'python3', mais 'python' ou 'py'
+#remplacer la commande au dessus par :
+
+#python main.py
+#py main.py
+
+
+
