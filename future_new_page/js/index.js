12caeaf4803794c307298427814a8c6d076e272a
nath54 <cerisara.nathan@gmail.com>
2020-08-22 00:00:33 +0200

    work on my new design of my website, that will be disponible soon

---
+++ future_new_page/js/index.js
@@ -0,0 +1,27 @@
+
+var dma = document.getElementById("myage");
+if(dma!=undefined){
+    var age=document.getElementById("datenaissance");
+    
+    var dt=new Date();
+    var monmois=dt.getMonth();
+    var monan=dt.getFullYear();
+    var monjour=dt.getDate();
+    
+    //alert(monan+" "+monmois+" "+monjour);
+    
+    var annais=2003
+    var moisnais=11
+    var journais=23
+    
+    var monage=monan-annais
+    if(monmois<moisnais){
+        monage-=1;
+    }
+    else if(monmois==moisnais && monjour<journais){
+        monage-=1;
+    }
+    dma.innerHTML=monage;
+}
+
+
