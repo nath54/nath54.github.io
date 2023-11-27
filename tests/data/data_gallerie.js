6c81373d7346fcaa423313d3aa76eb725406e871
nath54 <cerisara.nathan@gmail.com>
2023-11-27 15:18:23 +0100

    Add small pictures

--- tests/data/data_gallerie.js
+++ tests/data/data_gallerie.js
@@ -2,20 +2,84 @@
 
 var g_quotes = [
     {
-        "img_small": "",
-        "img": "https://drive.google.com/uc?id=1bqLFqopKUGuYAuTwFlNPWF-MdBer-d8M"
+        "img_small": "data/Gallerie/Citations/small_Believe you can and you_re halfway there..png",
+        "img": "https://drive.google.com/uc?id=19kYp7lpqkgKHeS3HnDsxoDM3zXwFsqZv"
     },
     {
-        "img_small": "",
-        "img": "https://drive.google.com/uc?id=1cgaejR8Z4aDfTTSbhnye_Hp4MKi4dS62"
+        "img_small": "data/Gallerie/Citations/small_deepest wounds .png",
+        "img": "https://drive.google.com/uc?id=15HaMOIAOIDb0kgwI4b3dw0mH3lpHbouj"
     },
     {
-        "img_small": "",
+        "img_small": "data/Gallerie/Citations/small_Don_t cry because its over smile because it happened.png",
         "img": "https://drive.google.com/uc?id=1FCZ9x0YYu7SvwXXxTjIu0Vm8X0YhTkGd"
     },
     {
+        "img_small": "data/Gallerie/Citations/small_drop in the ocean.png",
+        "img": "https://drive.google.com/uc?id=1znPhKElJIXL3EKC5BGwqCMvrSF7xija_"
+    },
+    {
+        "img_small": "data/Gallerie/Citations/small_Every man is guilty of all the good he did not do..png",
+        "img": "https://drive.google.com/uc?id=16GaLdljSBRo_wciAiu2wkkQ4tmC75yc2"
+    },
+    {
+        "img_small": "data/Gallerie/Citations/small_Everything you can imagine is real..png",
+        "img": "https://drive.google.com/uc?id=1LSbWbu9KURYKsukFcz3RMYo-iA0_NFyR"
+    },
+    {
+        "img_small": "data/Gallerie/Citations/small_excellence is an habit.png",
+        "img": "https://drive.google.com/uc?id=1Qi854O_V5f7-WKxliy_oMOeZWAmZbxk6"
+    },
+    {
         "img_small": "",
-        "img": "https://drive.google.com/uc?id=1q6DfSCA-G0h_-Les-u5LvT3ZGPREDxIv"
+        "img": "https://drive.google.com/uc?id=1rv8jRZeLvU4PPZBl7rNaPhsxzLEzrDzh"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1ifL3XaPRaa8zHLTkidG3O02Oe9U5WdGN"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1kQR3ihlTakSRvuLkGHafJq7XV5qfOV2_"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=112jEvef70dtSiWKtNaeIwoF_eHzfq1qf"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1Ue4d-DzbNSRS9Nk_jxcK2l4oq_awvAiK"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=15kio4DxODKjI2J3AERZFclI5toOpnIYv"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1PigI8CErkF7xDm854SIhEGR8vJw2e_Kn"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1HDjvBIP-J1Jlep2-hb38JkabMn_6vJq8"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1bCO_eiHcJD64bESYo8IIyxgVW_f-lheK"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1P-aANxvcc6SyHfZurd9v6_gwarWw_Geb"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1XnrtZ2meGggbZAvfEVlqumVyzW193xbD"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1dQ89UhCsEerFT_dYpTNn4gSMHwF2_901"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1PlrQZhkznI_Jnl34SByzWZUaPAMEdffR"
     },
     {
         "img_small": "",
@@ -23,11 +87,111 @@ var g_quotes = [
     },
     {
         "img_small": "",
-        "img": "https://drive.google.com/uc?id=1sjdDrBKSDsbNd43DEj12zuaGeEhlm30-"
+        "img": "https://drive.google.com/uc?id=1c1w61pPPrBazfRx6bZGNjeM1DK6rnlbW"
     },
     {
         "img_small": "",
-        "img": "https://drive.google.com/uc?id=1sWhxK0Gt9g89nluLhp7gM9bJsZ3n8PgH"
+        "img": "https://drive.google.com/uc?id=1W6kPjKKffeiuLiS2BxqSMVr7P43N_0Ne"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=18eb4lxZCPG7Y7EjNldMpIObLCExFb237"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1tcc97S598JeI-IvG3s38pJird7ui-JTv"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=12j_G6VorHcLVk_Y1fN1tTyiK9dpzQZPG"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1Qygoa8jmoi02CqWX1zF2dRpdmoKP5wWy"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1svcXE8B0Xe9iK1DIEttxc811uRcsYuRD"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=12TizX-CXcwcbdeGLzjQmcOBO1dL0EPVK"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1Oqn1KrTyQQuOlKqqwQKkOREFLiYkIQeU"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1rAaDjcrjN39GOvZVz2lXxkYZhUErS1Vc"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=17yXPqyz-yqj2vce4HAO5or6ZnXb_bJdb"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1DifRYVRM4rIcQfv1XdH3Nk7DIngfNUhY"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1J7omneT9wovq2PeRZg1NKaJMZoDQqUgx"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1sVOC-5vSqRIcGc1KiRoa9uRJ6N9cRGN0"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1bqLFqopKUGuYAuTwFlNPWF-MdBer-d8M"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1-KkS0Rs4qe_mj5Cc_h8barpIF7N0cTU4"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1-D8ktX1oOqXZG03WNfFTeqnUtaqUxsBm"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1P2Ert25JF0ntP1x4UAMbGWcIGouW1yfP"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1MGJfqf0_79RgZVpUwXMDja8maKgmKAq-"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1cgaejR8Z4aDfTTSbhnye_Hp4MKi4dS62"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1bKz8AgFMMZ2HpodWpawnS5vc4yg_TCID"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1HaikEOsERz3oC65GYITfAp2oDgtqs3dv"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1FbQ-X_xlnhrNlj0aqDkEi5z8QELzGcK7"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1nVJWzZIWZ7qqDcCCTbjZzVkZLxPZ3fmZ"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1ETj6xP1swBMO2wiyauPIEj5yE3vJlkFs"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1Oue66tIppveMRgKupNP63RUG9_XvyhmK"
+    },
+    {
+        "img_small": "",
+        "img": "https://drive.google.com/uc?id=1chDPFicMUNJl_B8tWHW1HYD9c_TKs7UU"
     },
     {
         "img_small": "",
@@ -35,259 +199,95 @@ var g_quotes = [
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1mkGb5lxm3gccPEXxbfqX0r2ljxMchdr6"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1tXoyloKnuP1OVm7OJjRji8_7Ju-gbkG1"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1oBdmgrwJ3yrJNngvmf4CPwFWOIoJK47e"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1GQHh6JRuqTCgyKbz2szU-usCxb8BhE9G"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=15MgQpfOQ79KDzNDSy4JY8wmVZxiHNH7_"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1sjdDrBKSDsbNd43DEj12zuaGeEhlm30-"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1tvXVo23D8UMHEK504yRDMSmPvOVzprbS"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1sWhxK0Gt9g89nluLhp7gM9bJsZ3n8PgH"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1KvMyroP7WKoaeRXF9hg2Yb9LgnvSsAS5"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1nWZ1lqJ2NUS_OQLODGSVhKiHoffOxdzX"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1JoZ-Sv8sA2vf4nwk-5b2bD4TsSI99zst"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=131wUAj_5PwhzS3F1WBQD3STC6-4xhaio"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1q6DfSCA-G0h_-Les-u5LvT3ZGPREDxIv"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1vwFds7I-UD-sbgAFSABJhBRDtFG8U-rP"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1GUHDY1UCDUSB8LeTdNJDtfZyCAdcuCrR"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1DnUl-xx2qELz6X8SdZgH1u1OJzoVlRD8"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1IEaue09hM8mC8b3Nja9BbJrAf710EBFu"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=19pn_HtbyYJzjGtRGzSCegRRofs_xK8i7"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1yygXfYBmmSQ6BoyrLqKSdQnBIxTr2gGt"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1T9K3zicFUxK9q4xAqnaOy7GbFxFs2PJE"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1qFMefHCQjk8cE_uBJgx2QPY9e8eYOmsc"
     },
     {
         "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1aKCpNXntyNmHu1qh2xlcy7KV5TxWjO0T"
     },
     {
         "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
-    },
-    {
-        "img_small": "",
-        "img": ""
+        "img": "https://drive.google.com/uc?id=1MfP0nhg2SZRotJmi_56hQYpKLl_ygdcz"
     }
 ];
 
