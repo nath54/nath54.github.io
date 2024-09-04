15a2145e22c20acf28c73b8b5e43af61cbf4dd36
Nathan Cerisara <cerisara.nathan@gmail.com>
2024-09-04 12:47:22 +0200

    début travail cv

--- js/sizes.js
+++ js/sizes.js
@@ -1,9 +1,25 @@
+// Get the arguments
+var current_url_string = window.location;
+var url = new URL(current_url_string);
+var size = url.searchParams.get("size");
 
 //
 const sizes = ["css/size_small.css", "css/size_normal.css", "css/size_large.css"];
 
 //
-window.current_size = 1;
+
+if(size != undefined && size >= 0 && size < sizes.length){
+    window.current_size = parseInt(size);
+}
+else{
+    window.current_size = 1;
+}
+
+//
+function setSize(){
+    //
+    document.getElementById("css_size_link").href = sizes[window.current_size];
+}
 
 
 //
@@ -11,7 +27,7 @@ function changeSize(){
     //
     window.current_size = (window.current_size + 1) % sizes.length;
     //
-    document.getElementById("css_size_link").href = sizes[window.current_size];
+    setSize();
 }
 
 
--- js/themes.js
+++ js/themes.js
@@ -1,17 +1,32 @@
+// Get the arguments
+var current_url_string = window.location;
+var url = new URL(current_url_string);
+var theme = url.searchParams.get("theme");
 
 //
 const themes = ["css/theme_light.css", "css/theme_dark.css", "css/theme_grayscale.css", "css/theme_grayscale_inversed.css"];
 
 //
-window.current_theme = 0;
 
+if(theme != undefined && theme >= 0 && theme < themes.length){
+    window.current_theme = parseInt(theme);
+}
+else{
+    window.current_theme = 0;
+}
+
+//
+function setTheme(){
+    //
+    document.getElementById("css_theme_link").href = themes[window.current_theme];
+}
 
 //
 function changeTheme(){
     //
     window.current_theme = (window.current_theme + 1) % themes.length;
     //
-    document.getElementById("css_theme_link").href = themes[window.current_theme];
+    setTheme();
 }
 
 
