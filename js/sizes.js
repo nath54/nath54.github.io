b18adb61c35342028fd548885c5833e1393856ad
Nathan Cerisara <cerisara.nathan@gmail.com>
2024-09-04 19:02:46 +0200

    Add localStorage for theme, lang and page zoom

--- js/sizes.js
+++ js/sizes.js
@@ -1,7 +1,7 @@
 // Get the arguments
 var current_url_string = window.location;
 var url = new URL(current_url_string);
-var size = url.searchParams.get("size");
+var size = url.searchParams.get("size") || localStorage.getItem("size") || 1;
 
 //
 const sizes = ["css/size_small.css", "css/size_normal.css", "css/size_large.css"];
@@ -26,6 +26,8 @@ function setSize(){
 function changeSize(){
     //
     window.current_size = (window.current_size + 1) % sizes.length;
+    //
+    localStorage.setItem("size", window.current_size);
     //
     setSize();
 }
