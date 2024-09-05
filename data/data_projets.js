d1beb6ca549d77bc9148cc783794dde226cdaea8
nath54 <cerisara.nathan@gmail.com>
2024-09-05 12:36:50 +0200

    Travail SiteWeb

---
+++ data/data_projets.js
@@ -0,0 +1,194 @@
+
+//
+const data_states = {
+    0: "not started",
+    1: "prototyped",
+    2: "in development",
+    3: "in development - beta version",
+    4: "done"
+};
+
+//
+const data_states_icons = {
+    // TODO
+};
+
+//
+const data_states_styles = {
+    // TODO
+};
+
+/*
+
+#!# Project Syntax #!#
+
+
+
+*/
+
+
+
+
+
+//
+const project_nalang = {
+    "name": "NaLang",
+    "description": {
+        "en": "A little app to learn new languages",
+        "fr": "Une petite application pour apprendre de nouvelles langues.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Godot Engine",
+    "icon": "data/data_projects/icons/icon_NaLang.svg",
+    "state": 3,
+    "objectives": {
+        "Fiche de vocabulaire custom pour les utilisateurs": 0,
+        "Ajouts d'exercices": 0,
+        "Bon module de text2speech multilangue sur toutes les plateformes d'exports": 0,
+        "Guide des leçons": 0
+    }
+};
+
+//
+const project_neavity_libs = {
+    "name": "Neavity Libs",
+    "description": {
+        "en": "Python & WebApp libs and tools for AI and creativity with templates.",
+        "fr": "Librairies & Outils en Python et WebApp pour l'IA et de la créativité avec des templates.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Python/HTML/JS/CSS",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 2,
+    "objectives": {
+    }
+};
+
+//
+const project_napp_engine = {
+    "name": "Napp Engine",
+    "description": {
+        "en": "C++ graphical app engine with android compilation support",
+        "fr": "Moteur d'applications graphiques en C++ avec support pour compilation android.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "C++",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 0,
+    "objectives": {
+    }
+};
+
+//
+const project_nadot = {
+    "name": "Nadot",
+    "description": {
+        "en": "An abstraction layer above the Godot Engine for easier game dev.",
+        "fr": "Une couche d'abstraction au-dessus de Godot Engine pour un développement de jeux-vidéos plus facile.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Godot Engine",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 0,
+    "objectives": {
+    }
+};
+
+//
+const project_napu = {
+    "name": "NaPU",
+    "description": {
+        "en": "Processing Units (CPU, GPU, NPU, ...) creator, optimiser and simulator",
+        "fr": "Créateur, Optimiseur et Simulateur d'Unités de Calculs (CPU, GPU, NPU, ...)",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Python/HTML/JS/CSS",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 0,
+    "objectives": {
+    }
+};
+
+//
+const project_namath = {
+    "name": "NaMath",
+    "description": {
+        "en": "A python formal calculator engine.",
+        "fr": "Moteur de calcul formel en python.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Python",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 1,
+    "objectives": {
+    }
+};
+
+//
+const project_nai_gamemaster = {
+    "name": "NAI GameMaster",
+    "description": {
+        "en": "An AI roleplaying WebApp made with Neavity Libs",
+        "fr": "Une WebApp pour du jeu de rôle par IA fait avec Neavity Libs.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Python/HTML/JS/CSS",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 0,
+    "objectives": {
+    }
+};
+
+//
+const project_nusic = {
+    "name": "Nusic",
+    "description": {
+        "en": "A music creator app with easy and unified partitions system.",
+        "fr": "Une application de création de musique avec un système de partitions unifiées.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "?",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 0,
+    "objectives": {
+    }
+};
+
+//
+const project_dedale_llm = {
+    "name": "Dedale LLM",
+    "description": {
+        "en": "An approach to MoE LLM with Python & Pytorch.",
+        "fr": "Une approche aux LLM MoE avec Python & Pytorch.",
+    },
+    "made_by": "Nathan Cerisara",
+    "made_with": "Python/Pytorch",
+    "icon": "data/data_projects/icons/no_icon.svg",
+    "state": 0,
+    "objectives": {
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//
+const projects = [
+    project_nalang,
+    project_neavity_libs,
+    project_napp_engine,
+    project_napu,
+    project_namath,
+    project_nai_gamemaster,
+    project_nadot,
+    project_nusic,
+    project_dedale_llm
+];
